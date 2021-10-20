98d174fc8349168e4b09c4fe40402b217614b111
a970747 <Matt.J.Jackson@gmail.com>
2021-10-19 21:20:39 -0400

    Add DriverContainer for dnd, rename props in Single components

--- frontend/components/SingleOrder.tsx
+++ frontend/components/SingleOrder.tsx
@@ -3,23 +3,23 @@ import Link from 'next/link';
 import { Order } from '../interfaces';
 
 type Props = {
-  data: Order
+  order: Order
 }
 
-const SingleOrder = ({ data }: Props) => {
+const SingleOrder = ({ order }: Props) => {
   return (
     <div className="my-2 hover:shadow-md hover:bg-blue-50">
-      <Link href={`/orders/${data.id}`}>
+      <Link href={`/orders/${order.id}`}>
         <a className="flex flex-col gap-2 border-2 border-gray-500 rounded p-2 md:grid md:grid-cols-10">
-          <p className="justify-self-center"><span className="inline md:hidden">OrderID: </span>{data.id}</p>
-          <p><span className="inline md:hidden">Driver: </span>{data.driver || 'TBD'}</p>
-          <p><span className="inline md:hidden">Start City: </span>{data.startCity}</p>
-          <p><span className="inline md:hidden">End City: </span>{data.endCity}</p>
-          <p><span className="inline md:hidden">Start Date: </span>{format(new Date(data.startDate), 'dd/MM/yy HH:mm')}</p>
-          <p><span className="inline md:hidden">End Date: </span>{format(new Date(data.endDate), 'dd/MM/yy HH:mm')}</p>
-          <p className="col-span-2"><span className="inline md:hidden">Description: </span>{data.description}</p>
-          <p className="text-green-700"><span className="inline md:hidden">Revenue: </span>$ {data.revenue}</p>
-          <p className="text-red-500"><span className="inline md:hidden">Cost: </span>$ {data.cost}</p>
+          <p className="justify-self-center"><span className="inline md:hidden">OrderID: </span>{order.id}</p>
+          <p><span className="inline md:hidden">Driver: </span>{order.driver || 'TBD'}</p>
+          <p><span className="inline md:hidden">Start City: </span>{order.startCity}</p>
+          <p><span className="inline md:hidden">End City: </span>{order.endCity}</p>
+          <p><span className="inline md:hidden">Start Date: </span>{format(new Date(order.startDate), 'dd/MM/yy HH:mm')}</p>
+          <p><span className="inline md:hidden">End Date: </span>{format(new Date(order.endDate), 'dd/MM/yy HH:mm')}</p>
+          <p className="col-span-2"><span className="inline md:hidden">Description: </span>{order.description}</p>
+          <p className="text-green-700"><span className="inline md:hidden">Revenue: </span>$ {order.revenue}</p>
+          <p className="text-red-500"><span className="inline md:hidden">Cost: </span>$ {order.cost}</p>
         </a>
       </Link>
     </div>
