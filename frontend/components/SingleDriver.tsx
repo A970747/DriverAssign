0976f8f82dc5bf437bb2ec77c908abadd3fbc937
a970747 <Matt.J.Jackson@gmail.com>
2021-10-19 21:19:35 -0400

    Name change for props in SingleOrder/Driver components

--- frontend/components/SingleDriver.tsx
+++ frontend/components/SingleDriver.tsx
@@ -2,25 +2,25 @@ import Link from 'next/link';
 import { Driver } from '../interfaces';
 
 type Props = {
-  data: Driver
+  driver: Driver
 }
 
-const SingleDriver = ({ data }: Props) => {
+const SingleDriver = ({ driver }: Props) => {
   /*   const handleClick = async (e) => {
     e.preventDefault();
     if (confirm('Are you sure you want to delete this item?')) {
-      const newData = swr?.data.filter((item) => item.id != data.id);
-      swr.mutate(newData);
-      await deleteOrder(data.id);
+      const newDriver = swr?.driver.filter((item) => item.id != driver.id);
+      swr.mutate(newdriver);
+      await deleteOrder(driver.id);
       swr.mutate();
     }
   }; */
 
   return (
     <div className="flex my-2 hover:shadow-md hover:bg-blue-50">
-      <Link href={`/drivers/${data.id}`}>
+      <Link href={`/drivers/${driver.id}`}>
         <a className="flex flex-col gap-2 border-2 border-gray-500 rounded p-2 md:grid md:grid-cols-10">
-          <p className="justify-self-center"><span className="inline md:hidden">Driver: </span>{data.fullName}</p>
+          <p className="justify-self-center"><span className="inline md:hidden">Driver: </span>{driver.fullName}</p>
         </a>
       </Link>
     </div>
