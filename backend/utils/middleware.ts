84783270fecbbbd89e9e29a46de12e49472bf6b4
a970747 <Matt.J.Jackson@gmail.com>
2021-10-21 15:06:55 -0400

    Reconfigure folders

---
+++ backend/utils/middleware.ts
@@ -0,0 +1,44 @@
+const logger = require('./logger');
+
+const requestLogger = (req, res, next) => {
+  logger.info('Method: ', req.method);
+  logger.info('Path: ', req.path);
+  logger.info('Body: ', req.body);
+  logger.info('---');
+  return next();
+};
+
+const tokenExtractor = (req, res, next) => {
+  const authorization = req.get('Authorization');
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    req.body.token = authorization.substring(7);
+  }
+
+  return next();
+};
+
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({ error: 'Unknown endpoint' });
+};
+
+const errorHandler = (error, req, res, next) => {
+  if (error.name === 'CastError') {
+    return res.status(400).send({ error: 'Malformatted  ID' });
+  };
+  if (error.name === 'ValidationError') {
+    return res.status(400).json({ error: error.message });
+  };
+  if (error.name === 'JsonWebTokenError') {
+    return res.status(401).json({ error: 'invalid token' });
+  };
+  logger.error(error.message);
+
+  return next(error);
+};
+
+module.exports = {
+  requestLogger,
+  tokenExtractor,
+  unknownEndpoint,
+  errorHandler,
+};
